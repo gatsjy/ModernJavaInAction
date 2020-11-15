3468221908d1fb9922648df83f159152d8763311
Gatsjy <gkswndks123@naver.com>
2020-11-15 19:00:00 +0900

    2020-11-15, 한주안, 람다식에 관한 공부

--- src/com/juan/algorithm/main/chap03/Sorting.java
+++ src/com/juan/algorithm/main/chap03/Sorting.java
@@ -2,7 +2,10 @@ package com.juan.algorithm.main.chap03;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+
+import static java.util.Comparator.comparing;
 
 public class Sorting {
 
@@ -10,7 +13,6 @@ public class Sorting {
     // 3.1 람다란 무엇인가?
     // 람다 표현식은 메서드로 전달할 수 있는 익명 함수를 단순화한 것이라고 할 수 있다.
     // 람다 표현식에는 이름은 없지만, 파라미터 리스트, 바디, 반환 형식, 발생할 수 있는 예외 리스트는 가질 수 있다.
-
     List<Apple> inventory = new ArrayList<>();
     inventory.addAll(Arrays.asList(
             new Apple(80, Color.GREEN),
@@ -18,21 +20,43 @@ public class Sorting {
             new Apple(120, Color.RED)
     ));
 
-    Runnable r1 = () -> System.out.println("Hello world 1");
+    // [Apple{color=GREEN, weight=80}, Apple{color=RED, weight=120}, Apple{color=GREEN, weight=155}]
+    inventory.sort(new AppleComparator());
+    //System.out.println(inventory);
 
-    Runnable r2 = new Runnable() {
+    inventory.set(1, new Apple(30, Color.GREEN));
+
+    //[Apple{color=GREEN, weight=30}, Apple{color=GREEN, weight=80}, Apple{color=GREEN, weight=155}]
+    inventory.sort(new Comparator<Apple>() {
       @Override
-      public void run() {
-        System.out.println("Hello world 2");
+      public int compare(Apple o1, Apple o2) {
+        return o1.getWeight() - o2.getWeight();
       }
-    };
+    });
+    System.out.println(inventory);
 
-    process(r1);
-    process(r2);
-    process(() -> System.out.println("Hello world 3"));
+    // reshuffling things a little
+    inventory.set(1, new Apple(20, Color.RED));
+
+    //[Apple{color=RED, weight=20}, Apple{color=GREEN, weight=30}, Apple{color=GREEN, weight=155}]
+    // 내부적으로 형을 유추해서 comparator을 돌려준다...
+    inventory.sort((a1, a2) -> a1.getWeight() - a2.getWeight());
+    System.out.println(inventory);
+
+    // reshuffling things a little
+    inventory.set(1, new Apple(10, Color.RED));
+
+    // ... 위의 람다식을 엄청나게 간단하게 줄여준다.
+    // asis inventory.sort((a1,a2) -> a1.getWeight() - a2.getWeight());
+    inventory.sort(comparing(Apple::getWeight));
+    System.out.println(inventory);
   }
 
-  public static void process(Runnable r){
-    r.run();
+  static class AppleComparator implements Comparator<Apple> {
+
+    @Override
+    public int compare(Apple o1, Apple o2) {
+      return o1.getWeight() - o2.getWeight();
+    }
   }
 }
