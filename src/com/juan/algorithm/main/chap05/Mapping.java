04ca76dd7ac08713d15464de804273fe19de2b5b
Gatsjy <gkswndks123@naver.com>
2020-11-17 23:21:00 +0900

    2020-11-17, 한주안, chap05 Mapping에 대한 공부

--- src/com/juan/algorithm/main/chap05/Mapping.java
+++ src/com/juan/algorithm/main/chap05/Mapping.java
@@ -20,34 +20,31 @@ public class Mapping {
 
     public static void main(String[] args) {
 
-        // 1. 메뉴의 이름들만 출력하세요
+        // 5.3 매핑
+        // 특정 객체에서 특정 데이터를 선택하는 작업은 데이터 처리 과정에서 자주 수행되는 연산이다.
+
+        // 5.3.1 스트림의 각 요소에 함수 적용하기
+        // 스트림은 함수를 인수로 받는 map 메서드를 지원한다. 인수로 제공된 함수는 각 요소에 적용되며 함수를 적용한 결과가 새로운 요소로 매핑된다.
         List<String> dishNames = menu.stream()
                 .map(Dish::getName)
                 .collect(toList());
         System.out.println(dishNames);
 
-        List<String> words = Arrays.asList("hello", "world");
-        List<Integer> wordlengths = words.stream()
+        List<String> words = Arrays.asList("Modern", "Java", "In", "Action");
+        List<Integer> wordLengths = words.stream()
                 .map(String::length)
                 .collect(toList());
-        System.out.println(wordlengths);
+        System.out.println(wordLengths);
 
-        // 2. flatMap
-        words.stream()
-                .flatMap((String line) -> Arrays.stream(line.split("")))
-                .distinct()
-                .forEach(System.out::println);
-
-        // 3. flatMap
-        List<Integer> numbers1 = Arrays.asList(1,2,3,4,5);
-        List<Integer> numbers2 = Arrays.asList(6,7,8);
-        List<int[]> pairs = numbers1.stream()
-                .flatMap((Integer i) -> numbers2.stream()
-                    .map((Integer j) -> new int[]{i,j}))
-                .filter(pair -> (pair[0] + pair[1]) % 3 == 0)
+        // 각 요리명의 길이를 알고 싶다면?
+        List<Integer> dishNameLengths = menu.stream()
+                .map(Dish::getName)
+                .map(String::length)
                 .collect(toList());
-        pairs.forEach(pair -> System.out.printf("(%d %d)", pair[0], pair[1]));
+        System.out.println(dishNameLengths);
 
-        int[] numbers = {5,0,2,7};
+        // 5.3.2 스트림 평면화
+        // 만약 중복되지 않는 고유 문자로 나타내고 싶다면?
+        // 첫번째 케이스
     }
 }
